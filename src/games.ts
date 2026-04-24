8d7c53d3952df1282b18f4f779eb9edec92a8d49
Lucas Rakotomalala <lucas.rakotomalala@yahoo.fr>
2026-04-24 12:02:56 +0200

    feat: add games module and update exports configuration

---
+++ src/games.ts
@@ -0,0 +1,15 @@
+// Copyright (c) Suigar
+// SPDX-License-Identifier: Apache-2.0
+
+export type { CoinSide } from './types/game.type';
+export type {
+	BuildCancelPvPCoinflipTransactionOptions,
+	BuildCoinflipTransactionOptions,
+	BuildCreatePvPCoinflipTransactionOptions,
+	BuildJoinPvPCoinflipTransactionOptions,
+	BuildLimboTransactionOptions,
+	BuildPlinkoTransactionOptions,
+	BuildRangeTransactionOptions,
+	BuildWheelTransactionOptions,
+	PvPCoinflipAction,
+} from './types/transaction-options.type';
